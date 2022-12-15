980b9bd23034dd826a270903ce5811b920260c10
Dickson Pang <34560707+dicksonpys@users.noreply.github.com>
2022-12-15 16:06:59 +0800

    redesigned demo widget

--- src/content/WidgetTerminal.tsx
+++ src/content/WidgetTerminal.tsx
@@ -1,11 +1,13 @@
+import classNames from "classnames";
 import React, { useEffect, useState } from "react";
 import JupButton from "src/components/JupButton";
+import LeftArrowIcon from "src/icons/LeftArrowIcon";
 import { IInit, WidgetPosition, WidgetSize } from "src/types";
 
 const WidgetTerminal = () => {
   const [isLoaded, setIsLoaded] = useState(false);
-  const [position, setPosition] = useState<WidgetPosition>('bottom-right');
-  const [size, setSize] = useState<WidgetSize>('default');
+  const [position, setPosition] = useState<WidgetPosition>("bottom-right");
+  const [size, setSize] = useState<WidgetSize>("default");
 
   useEffect(() => {
     let intervalId: NodeJS.Timeout | undefined = undefined;
@@ -18,8 +20,8 @@ const WidgetTerminal = () => {
         mode: "default",
         displayMode: "widget",
         widgetStyle: {
-          position: 'bottom-right',
-          size: 'default',
+          position: "bottom-right",
+          size: "default",
         },
         endpoint:
           "https://solana-mainnet.g.alchemy.com/v2/ZT3c4pYf1inIrB0GVDNR7nx4LwyED5Ci",
@@ -44,28 +46,130 @@ const WidgetTerminal = () => {
           "https://solana-mainnet.g.alchemy.com/v2/ZT3c4pYf1inIrB0GVDNR7nx4LwyED5Ci",
       });
     }
-  }, [position, size])
+  }, [position, size]);
 
   return (
     <div>
-      <p>Click to open the widget on the {position}.</p>
+      <div className="flex flex-col md:flex-row md:items-center justify-between mt-9 px-2 md:px-0">
+        <div>
+          <h2 className="font-semibold text-lg">Position & Size</h2>
+          <p className="text-white/30 text-xs">
+            Click the screen edge position to see how
+            <br />
+            it appear on your browser
+          </p>
+        </div>
 
-      <hr className="my-4" />
+        <div>
+          <div className="relative mt-9 md:mt-0">
+            <div className="bg-white/10 rounded-xl flex items-center justify-center w-full md:w-[384px] h-[216px]">
+              <span className="text-xs text-white/50">
+                Click edge to see position
+              </span>
 
-      <h3 className="font-semibold">Position</h3>
-      <div className='flex gap-2  flex-wrap mt-4'>
-        <JupButton onClick={() => { setPosition('bottom-left') }} type='button' className={position === 'bottom-left' ? '' : 'opacity-20 hover:opacity-70'}>bottom-left</JupButton>
-        <JupButton onClick={() => { setPosition('bottom-right') }} type='button' className={position === 'bottom-right' ? '' : 'opacity-20 hover:opacity-70'}>bottom-right</JupButton>
-        <JupButton onClick={() => { setPosition('top-left') }} type='button' className={position === 'top-left' ? '' : 'opacity-20 hover:opacity-70'}>top-left</JupButton>
-        <JupButton onClick={() => { setPosition('top-right') }} type='button' className={position === 'top-right' ? '' : 'opacity-20 hover:opacity-70'}>top-right</JupButton>
+              {/* Top left  */}
+              <div
+                className={classNames(
+                  "absolute left-1 top-1 cursor-pointer hover:bg-black/20 rounded-full p-1",
+                  {
+                    "jup-gradient": position === "top-left",
+                  }
+                )}
+                onClick={() => setPosition("top-left")}
+              >
+                <div className="rotate-45">
+                  <LeftArrowIcon width={24} height={24} />
+                </div>
+              </div>
+
+              {/* Top right  */}
+              <div
+                className={classNames(
+                  "absolute right-1 top-1 cursor-pointer hover:bg-black/20 rounded-full p-1",
+                  {
+                    "jup-gradient": position === "top-right",
+                  }
+                )}
+                onClick={() => setPosition("top-right")}
+              >
+                <div className="rotate-[135deg]">
+                  <LeftArrowIcon width={24} height={24} />
+                </div>
+              </div>
+
+              {/* Bottom left  */}
+              <div
+                className={classNames(
+                  "absolute left-1 bottom-1 cursor-pointer hover:bg-black/20 rounded-full p-1",
+                  {
+                    "jup-gradient": position === "bottom-left",
+                  }
+                )}
+                onClick={() => setPosition("bottom-left")}
+              >
+                <div className="-rotate-45">
+                  <LeftArrowIcon width={24} height={24} />
+                </div>
+              </div>
+
+              {/* Bottom right  */}
+              <div
+                className={classNames(
+                  "absolute right-1 bottom-1 cursor-pointer hover:bg-black/20 rounded-full p-1",
+                  {
+                    "jup-gradient": position === "bottom-right",
+                  }
+                )}
+                onClick={() => setPosition("bottom-right")}
+              >
+                <div className="rotate-[225deg]">
+                  <LeftArrowIcon width={24} height={24} />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <span className="text-sm font-semibold">Set Size</span>
+
+            <div className="space-x-2 p-1.5 mt-2 bg-black/30 rounded-xl">
+              <JupButton
+                size="sm"
+                onClick={() => {
+                  setSize("sm");
+                }}
+                type="button"
+                className={
+                  size === "sm" ? "bg-white/10" : "opacity-20 hover:opacity-70"
+                }
+              >
+                <div className="flex items-center space-x-2 text-xs">
+                  <div>Small</div>
+                </div>
+              </JupButton>
+              <JupButton
+                size="sm"
+                onClick={() => {
+                  setSize("default");
+                }}
+                type="button"
+                className={
+                  size === "default"
+                    ? "bg-white/10"
+                    : "opacity-20 hover:opacity-70"
+                }
+              >
+                <div className="flex items-center space-x-2 text-xs">
+                  <div>Default</div>
+                </div>
+              </JupButton>
+            </div>
+          </div>
+        </div>
       </div>
-      
-      <hr className="my-4" />
-      
-      <h3 className="font-semibold">Size</h3>
-      <div className='flex gap-2  flex-wrap mt-4'>
-        <JupButton onClick={() => { setSize('sm') }} type='button' className={size === 'sm' ? '' : 'opacity-20 hover:opacity-70'}>sm</JupButton>
-        <JupButton onClick={() => { setSize('default') }} type='button' className={size === 'default' ? '' : 'opacity-20 hover:opacity-70'}>default</JupButton>
+
+      <div className="mt-8 py-4">
+        <div className="border-b border-white/10" />
       </div>
     </div>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,68 +33,83 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [tab]);
 
   return (
-    <div className="bg-jupiter-dark-bg h-screen w-screen overflow-auto">
-      <AppHeader />
+    <div className="bg-jupiter-dark-bg h-screen w-screen overflow-auto flex flex-col justify-between">
+      <div>
+        <AppHeader />
 
-      <div className="flex">
-        <div className="flex flex-col items-center h-full w-full mt-4 md:mt-14">
-          <div className="flex flex-col justify-center items-center text-center">
-            <p className="text-[#9D9DA6]">Visual demo for integrating</p>
-            <SexyChameleonText className="text-[52px] font-semibold px-4 md:px-0">
-              Jupiter Terminal
-            </SexyChameleonText>
-          </div>
+        <div className="flex">
+          <div className="flex flex-col items-center h-full w-full mt-4 md:mt-14">
+            <div className="flex flex-col justify-center items-center text-center">
+              <p className="text-[#9D9DA6]">Visual demo for integrating</p>
+              <SexyChameleonText className="text-[52px] font-semibold px-4 md:px-0">
+                Jupiter Terminal
+              </SexyChameleonText>
+            </div>
 
-          <div className="space-x-2 p-1.5 mt-12 bg-black/30 rounded-xl">
-            <JupButton
-              onClick={() => {
-                setTab("modal");
-              }}
-              type="button"
-              className={tab === "modal" ? "bg-white/10" : "opacity-20 hover:opacity-70"}
-            >
-              <div className="flex items-center space-x-2 text-xs">
-                <TerminalModalIcon />
-                <div>Modal</div>
-              </div>
-            </JupButton>
-            <JupButton
-              onClick={() => {
-                setTab("integrated");
-              }}
-              type="button"
-              className={
-                tab === "integrated" ? "bg-white/10" : "opacity-20 hover:opacity-70"
-              }
-            >
-              <div className="flex items-center space-x-2 text-xs">
-                <TerminalIntegratedIcon />
-                <div>Integrated</div>
-              </div>
-            </JupButton>
-            <JupButton
-              onClick={() => {
-                setTab("widget");
-              }}
-              type="button"
-              className={tab === "widget" ? "bg-white/10" : "opacity-20 hover:opacity-70"}
-            >
-              <div className="flex items-center space-x-2 text-xs">
-                <TerminalWidgetIcon />
-                <div>Widget</div>
-              </div>
-            </JupButton>
-          </div>
+            <div className="space-x-2 p-1.5 mt-12 bg-black/30 rounded-xl">
+              <JupButton
+                size="sm"
+                onClick={() => {
+                  setTab("modal");
+                }}
+                type="button"
+                className={
+                  tab === "modal"
+                    ? "bg-white/10"
+                    : "opacity-20 hover:opacity-70"
+                }
+              >
+                <div className="flex items-center space-x-2 text-xs">
+                  <TerminalModalIcon />
+                  <div>Modal</div>
+                </div>
+              </JupButton>
+              <JupButton
+                size="sm"
+                onClick={() => {
+                  setTab("integrated");
+                }}
+                type="button"
+                className={
+                  tab === "integrated"
+                    ? "bg-white/10"
+                    : "opacity-20 hover:opacity-70"
+                }
+              >
+                <div className="flex items-center space-x-2 text-xs">
+                  <TerminalIntegratedIcon />
+                  <div>Integrated</div>
+                </div>
+              </JupButton>
+              <JupButton
+                size="sm"
+                onClick={() => {
+                  setTab("widget");
+                }}
+                type="button"
+                className={
+                  tab === "widget"
+                    ? "bg-white/10"
+                    : "opacity-20 hover:opacity-70"
+                }
+              >
+                <div className="flex items-center space-x-2 text-xs">
+                  <TerminalWidgetIcon />
+                  <div>Widget</div>
+                </div>
+              </JupButton>
+            </div>
 
-          <div className='w-full max-w-2xl px-2 md:px-0 text-white/75 mt-9 mb-16'>
-            {tab === "modal" ? (<ModalTerminal />) : null}
-            {tab === "integrated" ? (<IntegratedTerminal />) : null}
-            {tab === "widget" ? (<WidgetTerminal />) : null}
+            <div className="w-full max-w-3xl px-4 md:px-0 text-white/75 mt-9 mb-16">
+              {tab === "modal" ? <ModalTerminal /> : null}
+              {tab === "integrated" ? <IntegratedTerminal /> : null}
+              {tab === "widget" ? <WidgetTerminal /> : null}
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="fixed z-50 bottom-0 w-full bg-jupiter-bg">
+      <div className="w-full bg-jupiter-bg">
         <Footer />
       </div>
     </div>
