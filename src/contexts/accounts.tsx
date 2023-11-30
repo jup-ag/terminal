c16dc0a2e28187bff11bdefe9f28efea6ad7e077
Dickson P <34560707+dicksonpys@users.noreply.github.com>
2023-11-30 12:21:29 +0800

    fix non standard account being replaced (#47)

--- src/contexts/accounts.tsx
+++ src/contexts/accounts.tsx
@@ -1,12 +1,12 @@
-import BN from 'bn.js';
-import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
-import { PublicKey } from '@solana/web3.js';
-import React, { PropsWithChildren, useCallback, useContext, useEffect, useState } from 'react';
-import { useWalletPassThrough } from './WalletPassthroughProvider';
-import { WRAPPED_SOL_MINT } from 'src/constants';
-import { fromLamports } from 'src/misc/utils';
 import { useConnection } from '@jup-ag/wallet-adapter';
+import { AccountLayout, TOKEN_PROGRAM_ID, Token, AccountInfo as TokenAccountInfo, u64 } from '@solana/spl-token';
+import { AccountInfo, PublicKey } from '@solana/web3.js';
 import { useQuery } from '@tanstack/react-query';
+import BN from 'bn.js';
+import React, { PropsWithChildren, useCallback, useContext, useEffect, useState } from 'react';
+import { WRAPPED_SOL_MINT } from 'src/constants';
+import { fromLamports, getAssociatedTokenAddressSync } from 'src/misc/utils';
+import { useWalletPassThrough } from './WalletPassthroughProvider';
 
 const TOKEN_2022_PROGRAM_ID = new PublicKey('TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb');
 
@@ -59,6 +59,83 @@ const AccountContext = React.createContext<IAccountContext>({
   refresh: () => {},
 });
 
+export interface TokenAccount {
+  pubkey: PublicKey;
+  account: AccountInfo<Buffer>;
+  info: TokenAccountInfo;
+}
+
+export function wrapNativeAccount(pubkey: PublicKey, account: AccountInfo<Buffer>): TokenAccount | undefined {
+  if (!account) {
+    return undefined;
+  }
+
+  return {
+    pubkey: pubkey,
+    account,
+    info: {
+      address: pubkey,
+      mint: WRAPPED_SOL_MINT,
+      owner: pubkey,
+      amount: new u64(account.lamports.toString()),
+      delegate: null,
+      delegatedAmount: new u64(0),
+      isInitialized: true,
+      isFrozen: false,
+      isNative: true,
+      rentExemptReserve: null,
+      closeAuthority: null,
+    },
+  };
+}
+
+const deserializeAccount = (data: Buffer) => {
+  if (data == undefined || data.length == 0) {
+    return undefined;
+  }
+  const accountInfo = AccountLayout.decode(data);
+  accountInfo.mint = new PublicKey(accountInfo.mint);
+  accountInfo.owner = new PublicKey(accountInfo.owner);
+  accountInfo.amount = u64.fromBuffer(accountInfo.amount);
+  if (accountInfo.delegateOption === 0) {
+    accountInfo.delegate = null;
+    accountInfo.delegatedAmount = new u64(0);
+  } else {
+    accountInfo.delegate = new PublicKey(accountInfo.delegate);
+    accountInfo.delegatedAmount = u64.fromBuffer(accountInfo.delegatedAmount);
+  }
+  accountInfo.isInitialized = accountInfo.state !== 0;
+  accountInfo.isFrozen = accountInfo.state === 2;
+  if (accountInfo.isNativeOption === 1) {
+    accountInfo.rentExemptReserve = u64.fromBuffer(accountInfo.isNative);
+    accountInfo.isNative = true;
+  } else {
+    accountInfo.rentExemptReserve = null;
+    accountInfo.isNative = false;
+  }
+  if (accountInfo.closeAuthorityOption === 0) {
+    accountInfo.closeAuthority = null;
+  } else {
+    accountInfo.closeAuthority = new PublicKey(accountInfo.closeAuthority);
+  }
+  return accountInfo;
+};
+
+export const TokenAccountParser = (
+  pubkey: PublicKey,
+  info: AccountInfo<Buffer>,
+  programId: PublicKey,
+): TokenAccount | undefined => {
+  const tokenAccountInfo = deserializeAccount(info.data);
+
+  if (!tokenAccountInfo) return;
+  return {
+    pubkey,
+    account: info,
+    info: tokenAccountInfo,
+  };
+};
+
 const AccountsProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const { publicKey, connected } = useWalletPassThrough();
   const { connection } = useConnection();
@@ -87,8 +164,28 @@ const AccountsProvider: React.FC<PropsWithChildren> = ({ children }) => {
       ),
     );
 
+    const response = await connection.getTokenAccountsByOwner(
+      publicKey,
+      {
+        programId: TOKEN_PROGRAM_ID,
+      },
+      'confirmed',
+    );
+
+    const result = response.value.reduce((acc, { pubkey, account }) => {
+      const tokenAccount = TokenAccountParser(pubkey, account, TOKEN_PROGRAM_ID);
+      if (tokenAccount) {
+        acc.push(tokenAccount);
+      }
+      return acc;
+    }, new Array<TokenAccount>());
+
     const reducedResult = [...tokenAccounts.value, ...token2022Accounts.value].reduce(
       (acc, item: ParsedTokenData) => {
+        // Only allow standard TOKEN_PROGRAM_ID ATA
+        const expectedAta = getAssociatedTokenAddressSync(new PublicKey(item.account.data.parsed.info.mint), publicKey);
+        if (!expectedAta.equals(item.pubkey)) return acc;
+
         acc[item.account.data.parsed.info.mint] = {
           balance: item.account.data.parsed.info.tokenAmount.uiAmount,
           balanceLamports: new BN(0),
