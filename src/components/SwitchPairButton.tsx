f7bf801ff0058f4cd331c61794fbe83b98001c29
binhnguyen2501 <binhq6@gmail.com>
2023-12-10 19:17:39 +0700

    update build

--- src/components/SwitchPairButton.tsx
+++ src/components/SwitchPairButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const IconSwitchPairDark = (darkMode?: boolean) => (
+const IconSwitchPairDark = ({ darkMode }: { darkMode?: boolean }) => (
   <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M9.04393 5.74021L12 3.3701L9.04393 1V2.68839H1.0228V4.05189H9.04393V5.74021ZM2.95607 5.34607L0 7.71617L2.95607 10.0863V8.39789H10.9772V7.03439H2.95607V5.34607Z"
--- src/icons/RoutesSVG.tsx
+++ src/icons/RoutesSVG.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const RoutesSVG = ({ darkMode = false, width = '20', height = '20' }) => {
+export const RoutesSVG = ({ darkMode = false, width = 20, height = 20 }) => {
   return (
     <svg width={width} height={height} viewBox="0 0 7 9" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
