a23efdc75e9c0ac9feedb8015955e5a2e01fecfa
happyguy-michael <michael@raccoons.dev>
2025-06-26 00:30:16 +0800

    Use Lite API

--- src/contexts/USDValueProvider.tsx
+++ src/contexts/USDValueProvider.tsx
@@ -62,7 +62,7 @@ export const USDValueProvider: FC<PropsWithChildren<IInit>> = ({ children }) => 
 
   const getPriceFromJupAPI = useCallback(async (addresses: string[]) => {
     const { data }: { data: JupPriceResponse } = await fetch(
-      `https://api.jup.ag/price/v2?ids=${addresses.join(',')}`,
+      `https://lite-api.jup.ag/price/v2?ids=${addresses.join(',')}`,
     ).then((res) => res.json());
 
     const nowTimestamp = new Date().getTime();
