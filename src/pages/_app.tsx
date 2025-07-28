aeb98967147b8e1d9f9c3640e9dd3a44b12f655a
happyguy-michael <michael@raccoons.dev>
2025-07-28 12:56:36 +0800

    update copy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -254,7 +254,7 @@ export default function App() {
             </div>
           </div>
 
-          <div className="text-white grid  md:grid-cols-2 gap-4 px-2 mt-2 max-w-[700px] mx-auto">
+          <div className="text-white grid  md:grid-cols-2 gap-4 px-2 mt-4 max-w-[700px] mx-auto">
             <div className="bg-[#182220] rounded-xl p-4 relative h-[160px] flex flex-col gap-y-2 ">
               <div className="text-xl font-semibold">Swap fees</div>
               <div className="text-white/60 text-sm">Earn swap fees easily.</div>
@@ -279,8 +279,7 @@ export default function App() {
             <div className="bg-[#231B32] rounded-xl p-4 relative h-[160px] flex flex-col gap-y-2">
               <div className="text-xl font-semibold w-[80%]">RPC-less</div>
               <div className="text-white/60 w-[80%] text-sm">
-                Integrate Terminal without any RPCs, Ultra handles transaction sending, wallet balances and token
-                information.
+              All the Ultra goodness without any RPCs - we handle everything for you, including transaction sending
               </div>
               <img src="/upsell/rpc_less.svg" alt="swap-fees" className="absolute top-0 right-0" />
             </div>
