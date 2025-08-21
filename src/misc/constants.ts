f8c8e022f59ce797a0a8b2027011785133c74c3c
happyguy-michael <michael@raccoons.dev>
2025-08-22 02:37:30 +0800

    remove unused svg and cleanup

--- src/misc/constants.ts
+++
@@ -1,8 +0,0 @@
-import { PublicKey } from '@solana/web3.js';
-
-export const MINIMUM_SOL_BALANCE = 0.01;
-
-
-export const MAX_INPUT_LIMIT = 100_000_000_000_000;
-
-export const WRAPPED_SOL_MINT = new PublicKey('So11111111111111111111111111111111111111112');
